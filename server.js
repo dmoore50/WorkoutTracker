c3baf35b6675697634a76a8b328ebbb2e3bc9c68
dmoore50 <mooredustina850@gmail.com>
2024-07-30 16:44:28 -0400

    Commitment

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('./save-counter', (req, res) => {
+    const counter = req.body.counter;
+
+    fs.writeFile('counter.json', JSON.stringify({ counter }), (err) => {
+        if (err) {
+            res.status(500).send("Error saving counter");
+            return;
+        }
+        res.send('Counter saved');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+}); 
--- test.js
+++ test.js
@@ -8,4 +8,15 @@ testP.innerHTML = counter;
 testButton.addEventListener('click', () => {
     counter++;
     testP.innerHTML = counter;
-})
+
+    fetch('http://localhost:3000/save-counter', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ counter })
+    })
+        .then(response => response.text())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+}); 
